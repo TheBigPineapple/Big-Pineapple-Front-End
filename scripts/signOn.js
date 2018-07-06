676fa51905e9658f991fea667687122cbdbfc231
Samuel Ingram <samingram22@gmail.com>
2018-07-06 12:34:09 -0400

    Calendar Files and more Styling

---
+++ scripts/signOn.js
@@ -0,0 +1,10 @@
+let profile;
+
+// Might have to promise chain this with register habitica
+function onSignIn(googleUser) {
+	profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId());
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
