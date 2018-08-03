5eb444809115d65b33273606dc8394ba2874661b
skececi <sam.kececi@gmail.com>
2018-08-03 01:13:31 -0400

    fixed script

---
+++ scripts/processAddedHours.js
@@ -0,0 +1,33 @@
+function processAddedHours(addedHours) {
+    var CONFIG = require('./statsCONFIG.js');
+
+    var currentLvl = parseInt(getCookie('lvl'));
+    var currentExp = parseInt(getCookie('exp'));
+    var currentHours = parseInt(getCookie('hours'));
+    var currentHp = parseInt(getCookie('hp'));
+
+    // add the completed hours!
+    currentHours += addedHours;
+
+    // determine exp to level up
+    var expToLevelUp = EXP_TO_LEVEL_UP * currentLvl;
+
+    // add exp based on the hours completed!
+    currentExp += EXP_PER_HOUR * addedHours;
+
+    // level up!
+    while (currentExp > expToLevelUp) {
+        currentLvl++;
+        currentHp += HP_PER_LEVEL;
+        expToLevelUp = EXP_TO_LEVEL_UP * currentLvl;
+    }
+
+    updateCookies(currentLvl, currentExp, currentHours, currentHp);
+}
+
+function updateCookies(currentLvl, currentExp, currentHours, currentHp) {
+    setCookie('lvl', currentLvl);
+    setCookie('exp', currentExp);
+    setCookie('hours', currentHours);
+    setCookie('hp', currentHp);
+}
